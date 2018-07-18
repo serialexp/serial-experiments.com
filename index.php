4ff6d1a2902df7cfe35194be5a2623322e976090
Bart Riepe <aeolun@gmail.com>
2018-07-18 21:48:04 +0900

    Better home page, and hook to redeploy

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
             padding: 0.3vw;
             font-family: serif;
         }
+
+        img {
+            width: 100%;
+            height: auto;
+        }
     </style>
 </head>
 <body>
+<img src="serial-experiments-logo-small.png"/>
 Currently experimenting with having an empty homepage. If you have any feedback or comments, feel free to email bart
 [at] [this-domain].com.
 </body>
